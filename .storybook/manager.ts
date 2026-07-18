f06a94ccbff5f737cd2dd6c6935009c461e89faf
OJ Abba <phavorabba@gmail.com>
2026-07-18 01:42:13 +0100

    refactor: enhance Storybook configuration with custom theme

---
+++ .storybook/manager.ts
@@ -0,0 +1,28 @@
+import { addons } from 'storybook/manager-api'
+import { create } from 'storybook/theming'
+
+addons.setConfig({
+  theme: create({
+    base: 'light',
+    brandImage: './flux.svg',
+    brandTitle: 'Flux Design System',
+    brandTarget: '_self',
+    colorPrimary: '#323338',
+    colorSecondary: '#323338',
+    appBg: '#ffffff',
+    appContentBg: '#ffffff',
+    appBorderColor: '#e4e4e7',
+    appBorderRadius: 5,
+    barBg: '#ffffff',
+    barTextColor: '#18181b',
+    barHoverColor: '#ee1b78',
+    barSelectedColor: '#323338',
+    textColor: '#18181b',
+    inputBg: '#ffffff',
+    appHoverBg: '#efeff1',
+    inputTextColor: '#18181b',
+    fontBase: 'Inter, ui-sans-serif, system-ui, sans-serif',
+    fontCode: 'ui-monospace, SFMono-Regular, Menlo, monospace',
+  }),
+  sidebar: { showRoots: true },
+})
